cef0e93cc7f0bebe59acdabb176832978a4e3dae
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-11-08 20:33:38 +0000

    inicio del proyectos y algunos cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,35 @@
+import { useState } from "react";
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [input, setImput] = useState("")
+	const [elements, setElements] = useState([])
+
+	function cambioDeImput(e){
+		setImput(e.target.value)
+		console.log(e.target.value)
+
+};
+
+	function cambiarElement(){
+		setElements(elements.concat(input))
+		
+
+};
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="input-group mb-3">
+			<label></label>
+ 		 	<input value={input} type="text" onChange={cambioDeImput} className="form-control" aria-describedby="basic-addon1"/>
 		</div>
+		{elements.map( (element, index ) =><p key={index}> {element}</p>)}
+		<button onClick={cambiarElement}></button>
+		<p>{elements.length} shit to do</p>
+		</>
+
 	);
 };
 
