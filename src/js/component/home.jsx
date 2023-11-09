ff1a39af26320569226df97229473f70653bc2d0
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-11-09 18:41:10 +0000

    agregando la funcion con enter y el boton para elimar tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,26 +11,32 @@ const Home = () => {
 
 };
 
-	function cambiarElement(){
-		setElements(elements.concat(input))
-		
-
-};
-
-
-
-	return (
-		<>
-		<div className="input-group mb-3">
-			<label></label>
- 		 	<input value={input} type="text" onChange={cambioDeImput} className="form-control" aria-describedby="basic-addon1"/>
-		</div>
-		{elements.map( (element, index ) =><p key={index}> {element}</p>)}
-		<button onClick={cambiarElement}></button>
-		<p>{elements.length} shit to do</p>
-		</>
-
-	);
+function deleteElementById(index) {
+	setElements(elements.filter((element, i) => i !== index));
+  }
+  
+  function cambiarElement(){
+	  if(input !== "" ){
+		  setElements(elements.concat(input))}
+		  setImput("")
+		  elements.forEach((element, index) => {
+			  if (element === input) {
+				  deleteElementById(index);
+			  }
+		  });
+  };
+  
+  return (
+	  <>
+	  	  <h1>ToDos</h1>
+		  <div className="input-group mb-3">
+			  <label></label>
+				<input placeholder="What needs to be done?" onKeyDown={(e) => (e.keyCode === 13 ? cambiarElement(e) : null)} value={input} type="text" onChange={cambioDeImput} className="form-control" aria-describedby="basic-addon1"/>
+		  </div>
+		  {elements.map( (element, index ) => <p id={index} key={index}> {element} <button onClick={() => deleteElementById(index)}>Delete</button></p>)}
+		  <p>{elements.length} items left</p>
+	  </>
+  );
 };
 
 export default Home;
